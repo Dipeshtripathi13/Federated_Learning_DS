2f10fcc088b5466a8676151ef10ecb3cd937e415
Dipeshtripathi13 <killredps@gmail.com>
2024-12-01 22:09:04 -0600

    MVP created

--- evaluation.py
+++ evaluation.py
@@ -5,7 +5,7 @@ global_accuracies = []
 test_data = local_training.test_data
 aggregated_weights = aggrigate.aggregated_weights
 for X_test, y_test in test_data:
-    predictions = (X_test @ aggregated_weights.T).flatten()
+    predictions = (X_test @ aggregated_weights.T)
     predictions = (predictions > 0.5).astype(int)
     acc = accuracy_score(y_test, predictions)
     global_accuracies.append(acc)
